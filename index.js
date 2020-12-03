03a2ca1745b1a4b1ffa2863a87d2aa27cfa2e996
Dylan Crawshaw <dcrawsh@gmail.com>
2020-12-03 13:01:13 -0800

    add basic eventlistener to each drum

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ const middle = document.querySelector('.middle');
 const crash = document.querySelector('.crash');
 const high = document.querySelector('.high');
 
+document.addEventListener('keypress', (e) => {
+   if(e.key === 'k'){
+       console.log('k');
+   }
+   if(e.key === 's'){
+       console.log('s');
+   }
+   if(e.key === 'h'){
+       console.log('h');
+   }
+   if(e.key === 'c'){
+       console.log('c');
+   }
+   if(e.key === 'e'){
+       console.log('e');
+   }
+   if(e.key === 'm'){
+       console.log('m');
+   }
+   if(e.key === 'l'){
+       console.log('l');
+   }
+})
+
