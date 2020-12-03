7e3e3ac103676ae4a9b2e81033274022d1cca526
Dylan Crawshaw <dcrawsh@gmail.com>
2020-12-03 12:54:03 -0800

    select all drums from DOM

---
+++ index.js
@@ -0,0 +1,8 @@
+const snare = document.querySelector('.snare');
+const highHat = document.querySelector('.high-hat');
+const kick = document.querySelector('.kick');
+const low = document.querySelector('.low');
+const middle = document.querySelector('.middle');
+const crash = document.querySelector('.crash');
+const high = document.querySelector('.high');
+
